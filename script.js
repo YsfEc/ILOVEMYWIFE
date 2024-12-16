69baf569e1f7e18ba67b0c588269c53ac7222a3f
YsfEc <yalkin168@gmail.com>
2024-12-16 21:29:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function ButtonFunction() {
+    var x = document.getElementById("song");
+    document.querySelector(".ttotal").classList.add('body')
+    document.querySelector(".mainb").classList.add('button')
+    document.querySelector(".maintext").classList.add('h1')
+    document.querySelector(".svg").classList.add('svg2')
+    document.querySelector(".svg1").classList.add('svg2')
+    document.querySelector(".svg1").classList.add('svg22')
+    document.querySelector(".text").classList.add('text2')
+    x.play();
+}
